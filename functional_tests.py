b250babd77f401b63c3f3063cf2b08dbd8044eb9
Ingrid Lagares <ingridlagares@hotmail.com>
2021-02-26 18:55:26 -0300

    Criando BD de Produção

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+       table = self.browser.find_element_by_id('id_list_table')
+       rows = table.find_elements_by_tag_name('tr')
+       self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self): 
     
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
@@ -51,10 +56,7 @@ class NewVisitorTest(unittest.TestCase):
         # Ela visita a URL: a sua lista TODO ainda está armazenada
 
         # Satisfeita, ela vai dormir
-        
-        def check_for_row_in_list_table(self, row_text):
-           table = self.browser.find_element_by_id('id_list_table')
-           rows = table.find_elements_by_tag_name('tr')
-           self.assertIn(row_text, [row.text for row in rows])
+
+
 if __name__ == '__main__':
     unittest.main()
